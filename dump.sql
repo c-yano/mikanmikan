f1758904a20b3fcf4ed1f40d974eaf2f307d1921
IsamuKoseda <isamu.koseda@gmail.com>
2017-02-06 09:56:28 +0000

    issue #6で、初期状態からDatabaseとTableが作成されるように修正しました

--- dump.sql
+++ dump.sql
@@ -2,31 +2,44 @@
 --
 -- Host: 0.0.0.0    Database: Shop
 -- ------------------------------------------------------
--- Table structure for table `shop_product`
+
 --
+-- Drop all tables and database
+--
+USE `Shop`;
 
 DROP TABLE IF EXISTS `shop_product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+DROP TABLE IF EXISTS `shop_staff`;
+DROP DATABASE `Shop`;
+
+--
+-- Create Database and Set Privileges
+-- 
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `Shop` /*!40100 DEFAULT CHARACTER SET utf8 */;
+GRANT ALL PRIVILEGES ON `Shop`.* to shopadmin@localhost IDENTIFIED BY 'AdminPassword';
+
+-- 
+USE `Shop`;
+
+--
+-- Create Table `shop_product`
+--
 CREATE TABLE `shop_product` (
   `code` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
+  `name` varchar(45) NOT NULL,
   `price` int(11) DEFAULT NULL,
   `gazou` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
--- Table structure for table `shop_staff`
---
-
-DROP TABLE IF EXISTS `shop_staff`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `shop_staff` (
-  `code` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`code`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
--- Dump completed on 2017-02-06 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Createa Table `shop_staff`
+--
+CREATE TABLE `shop_staff` (
+  `code` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`code`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
